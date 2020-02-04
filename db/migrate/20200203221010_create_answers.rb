66f52145dba769ea97b75d566750fd3b69fc32c6
Roman Konoplev <roman@konoplev.pro>
2020-02-04 17:07:59 +0300

    Задание к уроку "Введение в  TDD/BDD" (#1)
    * Add model Question
    
    * Add model Answer

---
+++ db/migrate/20200203221010_create_answers.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateAnswers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :answers do |t|
+      t.references :question, foreign_key: true, null: false
+      t.text :body, null: false
+
+      t.timestamps
+    end
+  end
+end
