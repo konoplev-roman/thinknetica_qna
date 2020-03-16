de3e334e2b2d47b7d7ad3ee9a650ba7d2bef1c36
konoplev-roman <roman@konoplev.pro>
2020-03-16 19:29:24 +0300

    Refactor: add sorting by date of creation

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { is_expected.to belong_to(:user) }
-  it { is_expected.to have_many(:answers).order(best: :desc).dependent(:destroy) }
+  it { is_expected.to have_many(:answers).order(best: :desc, created_at: :desc).dependent(:destroy) }
 
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to validate_presence_of :body }
