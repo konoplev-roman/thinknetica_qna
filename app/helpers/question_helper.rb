017dd3ef5490f1445b82da9c802ea95231d71192
konoplev-roman <roman@konoplev.pro>
2020-03-17 19:59:40 +0300

    Refactor: delete unnecessary method and test

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -8,14 +8,4 @@ module QuestionHelper
       t('helpers.question.header.edit')
     end
   end
-
-  def question_form_options(question)
-    default_options = {}
-
-    # By default form submits are remote and unobtrusive XHRs.
-    # Disable remote submits with local: true
-    default_options.merge!(local: true) if question.new_record?
-
-    default_options
-  end
 end
--- spec/helpers/question_helper_spec.rb
+++ spec/helpers/question_helper_spec.rb
@@ -12,16 +12,4 @@ RSpec.describe QuestionHelper do
       expect(helper.question_header(create(:question))).to eq('Edit your question')
     end
   end
-
-  describe '#question_form_options' do
-    it 'return the option to disable remote for the create question form' do
-      # By default form submits are remote and unobtrusive XHRs.
-      # Disable remote submits with local: true
-      expect(helper.question_form_options(build(:question))).to eq(local: true)
-    end
-
-    it 'return an empty options for the edit question form' do
-      expect(helper.question_form_options(create(:question))).to be_empty
-    end
-  end
 end
