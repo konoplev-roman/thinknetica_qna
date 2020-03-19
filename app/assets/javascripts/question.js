58aa298a96ea3c298d373bfc3b1e72f111aef6e2
konoplev-roman <roman@konoplev.pro>
2020-03-19 23:24:31 +0300

    Refactor: move the template of question view to the partial

--- app/assets/javascripts/question.js
+++ app/assets/javascripts/question.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function() {
     $('.question').on('click', '.edit-question-link', function(event) {
         event.preventDefault();
 
-        $('.question').addClass('d-none');
+        $('.show-question').addClass('d-none');
         $('.edit-question').removeClass('d-none');
     })
 });
