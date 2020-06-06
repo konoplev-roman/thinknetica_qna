e1eac52a1c39fbce77344cf78b26889ebab8c345
Roman Konoplev <roman@konoplev.pro>
2020-06-06 11:24:05 +0300

    Remove unnecessary validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,6 +3,6 @@
 class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
-  validates :linkable, :name, :url, presence: true
+  validates :name, :url, presence: true
   validates :url, url: true
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 describe Link do
   it { is_expected.to belong_to(:linkable) }
 
-  it { is_expected.to validate_presence_of :linkable }
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :url }
   it { is_expected.to validate_url_of :url }
