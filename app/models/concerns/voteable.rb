546b812b3c8817b1114cf7472b9c74027b34c285
Roman Konoplev <roman@konoplev.pro>
2020-06-08 16:07:06 +0300

    Add rating method

--- app/models/concerns/voteable.rb
+++ app/models/concerns/voteable.rb
@@ -5,5 +5,9 @@ module Voteable
 
   included do
     has_many :votes, as: :voteable, dependent: :destroy, inverse_of: :voteable
+
+    def rating
+      votes.sum(:value)
+    end
   end
 end
--- spec/support/shared_examples/voteable.rb
+++ spec/support/shared_examples/voteable.rb
@@ -2,4 +2,17 @@
 
 shared_examples_for 'voteable' do
   it { is_expected.to have_many(:votes).dependent(:destroy) }
+
+  describe '#rating' do
+    let(:resource) { create(described_class.to_s.underscore.to_sym) }
+
+    before do
+      create_list(:vote, 10, voteable: resource, value: 1)
+      create_list(:vote, 3, voteable: resource, value: -1)
+    end
+
+    it 'returns the amount of votes' do
+      expect(resource.rating).to eq(7)
+    end
+  end
 end
